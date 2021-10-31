e2a1b9dfe2d839fe7c0be96710d7dfedfa8ceb65
liangsheng8708 <71660363+liangsheng8708@users.noreply.github.com>
2021-11-01 07:05:48 +0800

    Update Android.mk

--- init/Android.mk
+++ init/Android.mk
@@ -22,7 +22,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := \
     system/core/base/include \
     system/core/init
-LOCAL_SRC_FILES := init_pyxis.cpp
-LOCAL_MODULE := libinit_pyxis
+LOCAL_SRC_FILES := init_vela.cpp
+LOCAL_MODULE := libinit_vela
 
 include $(BUILD_STATIC_LIBRARY)
